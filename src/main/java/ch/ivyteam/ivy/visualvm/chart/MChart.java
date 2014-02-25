6361dc27493e9ed091eef7b1bb15298970ad58f2
nghia.tran <nghia.tran@a6661833-7379-1d48-9be0-c9d91705ca6e>
2014-02-25 02:37:07 +0000

    - Update polling interval runtime - Respect chart time period in VisualVM setting

--- src/main/java/ch/ivyteam/ivy/visualvm/chart/MChart.java
+++ src/main/java/ch/ivyteam/ivy/visualvm/chart/MChart.java
@@ -7,6 +7,7 @@ import ch.ivyteam.ivy.visualvm.view.IUpdatableUIObject;
 import com.sun.tools.visualvm.charts.ChartFactory;
 import com.sun.tools.visualvm.charts.SimpleXYChartDescriptor;
 import com.sun.tools.visualvm.charts.SimpleXYChartSupport;
+import com.sun.tools.visualvm.core.options.GlobalPreferences;
 import javax.swing.JComponent;
 
 /**
@@ -15,29 +16,31 @@ import javax.swing.JComponent;
  */
 class MChart implements IUpdatableUIObject {
 
-  private final SimpleXYChartSupport chart;
-  private final MChartDataSource fDataSource;
+    private final SimpleXYChartSupport chart;
+    private final MChartDataSource fDataSource;
 
-  MChart(MChartDataSource dataSource) {
-    fDataSource = dataSource;
-    SimpleXYChartDescriptor chartDescriptor = SimpleXYChartDescriptor.decimal(10, true, 1000);
-    dataSource.configureChart(chartDescriptor);
-    chart = ChartFactory.createSimpleXYChart(chartDescriptor);
-  }
+    MChart(MChartDataSource dataSource) {
+        fDataSource = dataSource;
+        int monitoredDataCache = GlobalPreferences.sharedInstance().getMonitoredDataCache();
+        SimpleXYChartDescriptor chartDescriptor = SimpleXYChartDescriptor.decimal(10, true,
+                60 * monitoredDataCache);
+        dataSource.configureChart(chartDescriptor);
+        chart = ChartFactory.createSimpleXYChart(chartDescriptor);
+    }
 
-  JComponent getUi() {
-    return chart.getChart();
-  }
+    JComponent getUi() {
+        return chart.getChart();
+    }
 
-  @Override
-  public void updateValues(MQueryResult result) {
-    long[] values = fDataSource.getValues(result);
-    chart.addValues(System.currentTimeMillis(), values);
-  }
+    @Override
+    public void updateValues(MQueryResult result) {
+        long[] values = fDataSource.getValues(result);
+        chart.addValues(System.currentTimeMillis(), values);
+    }
 
-  @Override
-  public void updateQuery(MQuery query) {
-    fDataSource.updateQuery(query);
-  }
+    @Override
+    public void updateQuery(MQuery query) {
+        fDataSource.updateQuery(query);
+    }
 
 }
