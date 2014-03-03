bbc9ecd6add372d675ad6403e01f6ed18a4719bb
tam.thai <tam.thai@a6661833-7379-1d48-9be0-c9d91705ca6e>
2014-03-03 08:28:18 +0000

    - Fix minor sonar warnings

--- src/main/java/ch/ivyteam/ivy/visualvm/chart/AbstractGaugePanel.java
+++ src/main/java/ch/ivyteam/ivy/visualvm/chart/AbstractGaugePanel.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ch.ivyteam.ivy.visualvm.chart;
 
 import ch.ivyteam.ivy.visualvm.chart.data.license.GaugeDataSource;
