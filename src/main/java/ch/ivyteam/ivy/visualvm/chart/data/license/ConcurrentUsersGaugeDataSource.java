5fc92b28e770d1f6353afeceba45e88ac7ed7193
nam.ha <nam.ha@a6661833-7379-1d48-9be0-c9d91705ca6e>
2014-03-04 08:40:29 +0000

    change color, use floor instead of round

--- src/main/java/ch/ivyteam/ivy/visualvm/chart/data/license/ConcurrentUsersGaugeDataSource.java
+++ src/main/java/ch/ivyteam/ivy/visualvm/chart/data/license/ConcurrentUsersGaugeDataSource.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * To change this license header, choose License Headers in Project Properties. To change this template file,
+ * choose Tools | Templates and open the template in the editor.
  */
 package ch.ivyteam.ivy.visualvm.chart.data.license;
 
@@ -16,10 +15,10 @@ public class ConcurrentUsersGaugeDataSource extends GaugeDataSource {
   public ConcurrentUsersGaugeDataSource(IDataBeanProvider dataBeanProvider, int serverSessionsLimit) {
     super(dataBeanProvider, IvyJmxConstant.IvyServer.SecurityManager.NAME,
             IvyJmxConstant.IvyServer.SecurityManager.KEY_LICENSED_SESSIONS);
-    Section greenSection = new Section(0, Math.round(serverSessionsLimit * 0.9), new Color(110, 184, 37));
-    Section orangeSection = new Section(Math.round(serverSessionsLimit * 0.9), serverSessionsLimit,
+    Section greenSection = new Section(0, Math.floor(serverSessionsLimit * 0.9), new Color(110, 184, 37));
+    Section orangeSection = new Section(Math.floor(serverSessionsLimit * 0.9), serverSessionsLimit,
             new Color(255, 210, 10));
-    Section redSection = new Section(serverSessionsLimit, Math.round(serverSessionsLimit * 1.5),
+    Section redSection = new Section(serverSessionsLimit, Math.floor(serverSessionsLimit * 1.5),
             new Color(240, 40, 40));
     getSections().add(greenSection);
     getSections().add(orangeSection);
--- src/main/java/ch/ivyteam/ivy/visualvm/chart/data/license/NamedUsersGaugeDataSource.java
+++ src/main/java/ch/ivyteam/ivy/visualvm/chart/data/license/NamedUsersGaugeDataSource.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * To change this license header, choose License Headers in Project Properties. To change this template file,
+ * choose Tools | Templates and open the template in the editor.
  */
 package ch.ivyteam.ivy.visualvm.chart.data.license;
 
@@ -16,10 +15,11 @@ public class NamedUsersGaugeDataSource extends GaugeDataSource {
   public NamedUsersGaugeDataSource(IDataBeanProvider dataBeanProvider, int namedUsersLimit) {
     super(dataBeanProvider, IvyJmxConstant.IvyServer.SecurityManager.NAME,
             IvyJmxConstant.IvyServer.SecurityManager.KEY_LICENSED_USERS);
-    Section greenSection = new Section(0, Math.round(namedUsersLimit * 0.9), Color.GREEN);
-    Section orangeSection = new Section(Math.round(namedUsersLimit * 0.9), Math.round(namedUsersLimit * 0.95),
-            Color.ORANGE);
-    Section redSection = new Section(Math.round(namedUsersLimit * 0.95), namedUsersLimit, Color.RED);
+    Section greenSection = new Section(0, Math.floor(namedUsersLimit * 0.9), new Color(110, 184, 37));
+    Section orangeSection = new Section(Math.floor(namedUsersLimit * 0.9),
+            Math.floor(namedUsersLimit * 0.95), new Color(255, 210, 10));
+    Section redSection = new Section(Math.floor(namedUsersLimit * 0.95), namedUsersLimit, new Color(240, 40,
+            40));
     getSections().add(greenSection);
     getSections().add(orangeSection);
     getSections().add(redSection);
