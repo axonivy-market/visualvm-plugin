242c3fac38de014b139981e5d5f52cffd6c16b33
nghia.tran <nghia.tran@a6661833-7379-1d48-9be0-c9d91705ca6e>
2014-03-12 03:21:08 +0000

    Test for External databases story

---
+++ src/test/java/ch/ivyteam/ivy/visualvm/test/datasource/externaldb/ExternalDbConnectionChartDataSourceTest.java
@@ -0,0 +1,88 @@
+package ch.ivyteam.ivy.visualvm.test.datasource.externaldb;
+
+import ch.ivyteam.ivy.visualvm.chart.Query;
+import ch.ivyteam.ivy.visualvm.chart.QueryResult;
+import ch.ivyteam.ivy.visualvm.chart.data.externaldb.ExternalDbConnectionChartDataSource;
+import ch.ivyteam.ivy.visualvm.model.IvyJmxConstant;
+import ch.ivyteam.ivy.visualvm.test.AbstractTest;
+import static ch.ivyteam.ivy.visualvm.test.AbstractTest.addTestData;
+import static ch.ivyteam.ivy.visualvm.test.AbstractTest.createMockConnection;
+import ch.ivyteam.ivy.visualvm.test.data.model.BeanTestData;
+import ch.ivyteam.ivy.visualvm.test.util.TestUtil;
+import ch.ivyteam.ivy.visualvm.view.IDataBeanProvider;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import javax.management.AttributeNotFoundException;
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanException;
+import javax.management.MBeanServerConnection;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import javax.management.ReflectionException;
+import javax.xml.bind.JAXBException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.mockito.Mockito.when;
+
+@RunWith(Parameterized.class)
+public class ExternalDbConnectionChartDataSourceTest extends AbstractTest {
+  @Parameterized.Parameters(name = "{index}")
+  public static Iterable<Object[]> data() throws JAXBException, URISyntaxException {
+    return TestUtil.createTestData(
+            "/ch/ivyteam/ivy/visualvm/test/datasource/externaldb/ExternalDbConnectionChartDataSourceTest.xml",
+            new Object[]{new String[]{"app1", "env1", "config1"}, 5, 1, 0},
+            new Object[]{new String[]{"app1", "env1", "config2"}, 5, 1, 1},
+            new Object[]{new String[]{"app1", "env2", "config1"}, 5, 0, 0}
+    );
+  }
+
+  private final String[] fConfigs;
+  private final int fMaxConnection;
+  private final int fOpenConnection;
+  private final int fUsedConnection;
+
+  public ExternalDbConnectionChartDataSourceTest(BeanTestData.Dataset dataset,
+          String[] configs, int maxConnection, int openConnection, int usedConnection) {
+    super(dataset);
+    fConfigs = new String[configs.length];
+    System.arraycopy(configs, 0, fConfigs, 0, configs.length);
+    fMaxConnection = maxConnection;
+    fOpenConnection = openConnection;
+    fUsedConnection = usedConnection;
+  }
+
+  @Test
+  public void testConnectionChartDataSource() throws MalformedObjectNameException, IOException,
+          ReflectionException, MBeanException, AttributeNotFoundException, InstanceNotFoundException {
+    final MBeanServerConnection mockConnection = createMockConnection();
+    addTestData(mockConnection, getDataset());
+    ObjectName objectName = new ObjectName("Xpert.ivy Server:type=External Database,application="
+            + fConfigs[0] + ",environment=" + fConfigs[1] + ",name=" + fConfigs[2]);
+    when(mockConnection.getAttribute(objectName,
+            IvyJmxConstant.IvyServer.ExternalDatabase.KEY_MAX_CONNECTION)).thenReturn(fMaxConnection);
+
+    IDataBeanProvider provider = new IDataBeanProvider() {
+      @Override
+      public MBeanServerConnection getMBeanServerConnection() {
+        return mockConnection;
+      }
+
+    };
+
+    ExternalDbConnectionChartDataSource dataSource = new ExternalDbConnectionChartDataSource(provider, "",
+            "", "");
+    dataSource.setApplication(fConfigs[0]);
+    dataSource.setEnvironment(fConfigs[1]);
+    dataSource.setConfigName(fConfigs[2]);
+    dataSource.init();
+
+    Query query = new Query();
+    dataSource.updateQuery(query);
+    QueryResult result = query.execute(mockConnection);
+    long[] values = dataSource.getValues(result);
+    assertEquals(fOpenConnection, values[0]);
+    assertEquals(fUsedConnection, values[1]);
+  }
+
+}
---
+++ src/test/java/ch/ivyteam/ivy/visualvm/test/datasource/externaldb/ExternalDbTransactionChartDataSourceTest.java
@@ -0,0 +1,84 @@
+package ch.ivyteam.ivy.visualvm.test.datasource.externaldb;
+
+import ch.ivyteam.ivy.visualvm.chart.Query;
+import ch.ivyteam.ivy.visualvm.chart.QueryResult;
+import ch.ivyteam.ivy.visualvm.chart.data.externaldb.ExternalDbTransactionChartDataSource;
+import ch.ivyteam.ivy.visualvm.test.AbstractTest;
+import static ch.ivyteam.ivy.visualvm.test.AbstractTest.addTestData;
+import static ch.ivyteam.ivy.visualvm.test.AbstractTest.createMockConnection;
+import ch.ivyteam.ivy.visualvm.test.data.model.BeanTestData;
+import ch.ivyteam.ivy.visualvm.test.util.TestUtil;
+import ch.ivyteam.ivy.visualvm.view.IDataBeanProvider;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import javax.management.AttributeNotFoundException;
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanException;
+import javax.management.MBeanServerConnection;
+import javax.management.MalformedObjectNameException;
+import javax.management.ReflectionException;
+import javax.xml.bind.JAXBException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(Parameterized.class)
+public class ExternalDbTransactionChartDataSourceTest extends AbstractTest {
+  @Parameterized.Parameters(name = "{index}")
+  public static Iterable<Object[]> data() throws JAXBException, URISyntaxException {
+    return TestUtil.createTestData(
+            "/ch/ivyteam/ivy/visualvm/test/datasource/externaldb/ExternalDbTransChartDataSourceTest.xml",
+            new Object[]{new String[]{"app1", "env1", "config1"}, 0, 0},
+            new Object[]{new String[]{"app1", "env1", "config1"}, 2, 1},
+            new Object[]{new String[]{"app1", "env1", "config1"}, 1, 0}
+    );
+  }
+
+  private static IDataBeanProvider fProvider;
+  private static Query fQuery;
+  private static ExternalDbTransactionChartDataSource fDataSource;
+
+  private final String[] fConfigs;
+  private final int fTransactions;
+  private final int fErrors;
+
+  public ExternalDbTransactionChartDataSourceTest(BeanTestData.Dataset dataset,
+          String[] configs, int transactions, int errors) {
+    super(dataset);
+    fConfigs = new String[configs.length];
+    System.arraycopy(configs, 0, fConfigs, 0, configs.length);
+    fTransactions = transactions;
+    fErrors = errors;
+  }
+
+  @Test
+  public void testConnectionChartDataSource() throws MalformedObjectNameException, IOException,
+          ReflectionException, MBeanException, AttributeNotFoundException, InstanceNotFoundException {
+    final MBeanServerConnection mockConnection = createMockConnection();
+    addTestData(mockConnection, getDataset());
+    if (fProvider == null) {
+      fProvider = mock(IDataBeanProvider.class);
+    }
+    when(fProvider.getMBeanServerConnection()).thenReturn(mockConnection);
+
+    if (fDataSource == null) {
+      fDataSource
+              = new ExternalDbTransactionChartDataSource(fProvider, "",
+                      "", "");
+      fDataSource.setApplication(fConfigs[0]);
+      fDataSource.setEnvironment(fConfigs[1]);
+      fDataSource.setConfigName(fConfigs[2]);
+      fDataSource.init();
+
+      fQuery = new Query();
+      fDataSource.updateQuery(fQuery);
+    }
+    QueryResult result = fQuery.execute(mockConnection);
+    long[] values = fDataSource.getValues(result);
+    assertEquals(fTransactions, values[0]);
+    assertEquals(fErrors, values[1]);
+  }
+
+}
