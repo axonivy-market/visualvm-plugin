b1b92d0c2b48584d99c1db046c2b42327baa6084
hoang.le <hoang.le@a6661833-7379-1d48-9be0-c9d91705ca6e>
2014-03-12 05:53:09 +0000

    -revert MaxDeltaValueChartLabelCalcSupport.java

---
+++ src/main/java/ch/ivyteam/ivy/visualvm/chart/data/support/MaxDeltaValueChartLabelCalcSupport.java
@@ -0,0 +1,36 @@
+package ch.ivyteam.ivy.visualvm.chart.data.support;
+
+import ch.ivyteam.ivy.visualvm.chart.QueryResult;
+import javax.management.ObjectName;
+
+public class MaxDeltaValueChartLabelCalcSupport extends DeltaValueChartLabelCalcSupport {
+  private long fMaxDelta = 0;
+
+  public MaxDeltaValueChartLabelCalcSupport(String text, ObjectName objName, String attrKey) {
+    super(text, objName, attrKey);
+  }
+
+  @Override
+  protected long calculateValue(QueryResult queryResult) {
+    long nextDelta = super.calculateValue(queryResult);
+    long result = fMaxDelta;
+    if (nextDelta > result) {
+      result = nextDelta;
+    }
+    return result;
+  }
+
+  @Override
+  public void updateValues(QueryResult queryResult) {
+    if (isLastValueValid()) {
+      long nextDelta = calculateValue(queryResult);
+      if (nextDelta > fMaxDelta) {
+        fMaxDelta = nextDelta;
+      }
+    } else {
+      fMaxDelta = 0;
+    }
+    super.updateValues(queryResult);
+  }
+
+}
